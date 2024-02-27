c1116d9b50d0bcaf254f5bc860f1f1ea87f15de1
Ilia Kozachyshyn <kozachishin543@gmail.com>
2024-02-27 19:38:19 +0200

    new commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Links from "./links/Links";
+import "./sidebar.scss";
+import ToggleButton from "./toggleButton/ToggleButton";
+import { motion } from "framer-motion";
+
+const variants = {
+  opened: {
+    opacity: 1,
+    clipPath: "circle(3000px at 50px 50px)",
+    transition: { type: "spring", stiffness: 10, delay: 0.1 },
+  },
+  closed: {
+    opacity: 1,
+    clipPath: "circle(30px at 50px 50px)",
+    transition: { delay: 0.1, type: "spring", stiffness: 300, damping: 40 },
+  },
+};
+
+function Sidebar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <motion.div className="sidebar" animate={open ? "opened" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/sidebar/links/Links.jsx
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  opened: {
+    transition: { staggerChildren: 0.15 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.06, staggerDirection: -1 },
+  },
+};
+const itemVariants = {
+  opened: {
+    y: 0,
+    opacity: 1,
+    transition: {},
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
+function Links() {
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
+  return (
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+}
+
+export default Links;
