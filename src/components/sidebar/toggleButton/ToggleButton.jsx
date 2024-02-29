ea9985aa0ca29cf38a7cebeb63e0ade9dd6be431
Ilia Kozachyshyn <kozachishin543@gmail.com>
2024-02-29 22:22:27 +0200

    menu button fixed

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -8,6 +8,7 @@ function ToggleButton({ setOpen }) {
           strokeWidth="3"
           stroke="#324b4a"
           strokeLinecap="round"
+          d="M 2 2.5 L 20 2.5"
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             opened: { d: "M 3 16.5 L 17 2.5" },
@@ -27,6 +28,7 @@ function ToggleButton({ setOpen }) {
           strokeWidth="3"
           stroke="#324b4a"
           strokeLinecap="round"
+          d="M 2 16.346 L 20 16.346"
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             opened: { d: "M 3 2.5 L 17 16.346" },
