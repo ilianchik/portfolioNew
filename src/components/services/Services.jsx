7d5a9ec4ebf5b8da586c8b80389695f764db2e8e
Ilia Kozachyshyn <kozachishin543@gmail.com>
2024-03-01 17:06:55 +0200

    safari final

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -21,69 +21,71 @@ function Services() {
   const [t] = useTranslation("global");
 
   return (
-    <motion.div
-      className="services"
-      variants={variants}
-      initial="initial"
-      whileInView="animate"
-    >
-      <motion.div className="textContainer" variants={variants}>
-        <p>
-          {t("Services.topText1")} <br /> {t("Services.topText2")}
-        </p>
-        <hr />
+    <div className="div">
+      <motion.div
+        className="services"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+      >
+        <motion.div className="textContainer" variants={variants}>
+          <p>
+            {t("Services.topText1")} <br /> {t("Services.topText2")}
+          </p>
+          <hr />
+        </motion.div>
+        <motion.div className="titleContainer">
+          <div className="title">
+            <img src="/people.webp" alt="title img" />
+            <h1>
+              <motion.b whileHover={{ color: "#324b4a" }}>
+                {t("Services.headingText1.1")}
+              </motion.b>{" "}
+              {t("Services.headingText1.2")}
+            </h1>
+          </div>
+          <div className="title">
+            <h1>
+              <motion.b whileHover={{ color: "#324b4a" }}>
+                {t("Services.headingText2.1")}{" "}
+              </motion.b>
+              {t("Services.headingText2.2")}
+            </h1>
+            <button>{t("Services.headingButton")}</button>
+          </div>
+        </motion.div>
+        <motion.div className="listContainer" variants={variants}>
+          <motion.div
+            className="box"
+            whileHover={{ backgroundColor: "#ebeded", color: "black" }}
+          >
+            <h2> {t("Services.box1Heading")}</h2>
+            <p>{t("Services.box1Text")}</p>
+          </motion.div>
+          <motion.div
+            className="box"
+            whileHover={{ backgroundColor: "#ebeded", color: "black" }}
+          >
+            <h2> {t("Services.box2Heading")}</h2>
+            <p>{t("Services.box2Text")}</p>
+          </motion.div>
+          <motion.div
+            className="box"
+            whileHover={{ backgroundColor: "#ebeded", color: "black" }}
+          >
+            <h2> {t("Services.box3Heading")}</h2>
+            <p>{t("Services.box3Text")}</p>
+          </motion.div>
+          <motion.div
+            className="box"
+            whileHover={{ backgroundColor: "#ebeded", color: "black" }}
+          >
+            <h2> {t("Services.box4Heading")}</h2>
+            <p>{t("Services.box4Text")}</p>
+          </motion.div>
+        </motion.div>
       </motion.div>
-      <motion.div className="titleContainer">
-        <div className="title">
-          <img src="/people.webp" alt="title img" />
-          <h1>
-            <motion.b whileHover={{ color: "#324b4a" }}>
-              {t("Services.headingText1.1")}
-            </motion.b>{" "}
-            {t("Services.headingText1.2")}
-          </h1>
-        </div>
-        <div className="title">
-          <h1>
-            <motion.b whileHover={{ color: "#324b4a" }}>
-              {t("Services.headingText2.1")}{" "}
-            </motion.b>
-            {t("Services.headingText2.2")}
-          </h1>
-          <button>{t("Services.headingButton")}</button>
-        </div>
-      </motion.div>
-      <motion.div className="listContainer" variants={variants}>
-        <motion.div
-          className="box"
-          whileHover={{ backgroundColor: "#ebeded", color: "black" }}
-        >
-          <h2> {t("Services.box1Heading")}</h2>
-          <p>{t("Services.box1Text")}</p>
-        </motion.div>
-        <motion.div
-          className="box"
-          whileHover={{ backgroundColor: "#ebeded", color: "black" }}
-        >
-          <h2> {t("Services.box2Heading")}</h2>
-          <p>{t("Services.box2Text")}</p>
-        </motion.div>
-        <motion.div
-          className="box"
-          whileHover={{ backgroundColor: "#ebeded", color: "black" }}
-        >
-          <h2> {t("Services.box3Heading")}</h2>
-          <p>{t("Services.box3Text")}</p>
-        </motion.div>
-        <motion.div
-          className="box"
-          whileHover={{ backgroundColor: "#ebeded", color: "black" }}
-        >
-          <h2> {t("Services.box4Heading")}</h2>
-          <p>{t("Services.box4Text")}</p>
-        </motion.div>
-      </motion.div>
-    </motion.div>
+    </div>
   );
 }
 
